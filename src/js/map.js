070e1b288398738061fef96118ca339d6056cfd7
Vladimir Shkerin <vladshkerin@gmail.com>
2019-08-16 14:31:00 +0300

    Changed api key and position for Yandex.Map

--- src/js/map.js
+++ src/js/map.js
@@ -6,16 +6,17 @@
     const btnOpen = document.querySelector('.contacts-list__item:first-child .contact__value');
     const btnClose = document.querySelector('.map .btn-close');
     const shadow = document.querySelector('.shadow');
+    let coords = [54.926766, 58.158243];
     let map;
 
     function initMap() {
         if (!map) {
             map = new ymaps.Map('map-area', {
-                center: [54.929539, 58.169966],
+                center: coords,
                 zoom: 16
             });
 
-            let placemark = new ymaps.Placemark([54.929539, 58.169966]);
+            let placemark = new ymaps.Placemark(coords);
             map.geoObjects.add(placemark);
         }
     }
